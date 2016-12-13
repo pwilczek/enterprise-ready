153df2c1337f0e115befb6260bbf07fa930fc052
Piotr <piotr.wilczek@wilu.name>
2016-12-13 23:17:43 +0100

    Post commnet to rest endpoint...

--- comments-service/src/main/java/name/wilu/era/comments/CommentsController.java
+++ comments-service/src/main/java/name/wilu/era/comments/CommentsController.java
@@ -1,16 +1,18 @@
 package name.wilu.era.comments;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CommentsController {
 
-    @RequestMapping(value = "/run/{v}")
-    public String serve(@PathVariable("v") String v) {
-        System.out.println("Serving incoming request!");
-        return v + v;
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String add(@RequestParam("comment") String comment) {
+        //TODO introduce some enterprise logging
+        System.out.println("Incoming new comment to handle! -> " + comment);
+        return "cool!";
     }
 
 }
